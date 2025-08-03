481e9b9caacbed94d0292c65a9387b7ffd6bad89
IPashkaYouNot <51509642+IPashkaYouNot@users.noreply.github.com>
2025-08-03 13:54:26 +0300

    Update temp.lua

--- temp.lua
+++ temp.lua
@@ -7,6 +7,10 @@ return ({
     o1 = function(o)
       local z = getfenv()
       for key, value in pairs(z) do
+            if type(value) == userdata then
+                    print("Hello")
+                    print(inspect(getmetatable(value)))
+            end
             print(key, value, type(value), typeof(value))
       end
       return(print)
