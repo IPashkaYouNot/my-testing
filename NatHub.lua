6c42db15d0f5c5d02b40efe7b096244ea1da4469
IPashkaYouNot <51509642+IPashkaYouNot@users.noreply.github.com>
2025-08-03 11:35:34 +0300

    Update NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -1,4 +1,4 @@
-repeat wait() until game:IsLoaded() and game:FindFirstChild("CoreGui") and pcall(function() return game.CoreGui end)
-local game_url = game:HttpGet("https://raw.githubusercontent.com/IPashkaYouNot/my-testing/refs/heads/master/65c66a87b33565a9dea1a54b798b6b2a.lua")
+-- repeat wait() until game:IsLoaded() and game:FindFirstChild("CoreGui") and pcall(function() return game.CoreGui end)
+-- local game_url = game:HttpGet("https://raw.githubusercontent.com/IPashkaYouNot/my-testing/refs/heads/master/65c66a87b33565a9dea1a54b798b6b2a.lua")
 -- loadstring(game_url)
-
+print("Hello")
